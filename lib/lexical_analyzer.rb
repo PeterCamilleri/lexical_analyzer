730117b92f9334145ad67581986c2ba9243297fa
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-08-24 23:38:27 -0400

    Refined the analyzer to avoid skipping entries when a token is suppressed.

--- lib/lexical_analyzer.rb
+++ lib/lexical_analyzer.rb
@@ -28,7 +28,7 @@ class LexicalAnalyzer
       if match_data = text.match(rule[1])
         @text = match_data.post_match
         token = (rule[2] || DTB).call(rule[0], match_data.to_s)
-        return token if token
+        return token || get
       end
 
     end
